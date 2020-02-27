c750d0e39f1ade3f1482b5a789a61cfe80b3b1dc
sean.meng <Sean.Meng@lairdconnect.com>
2020-02-27 15:48:13 -0500

    weblcm: enable factory reset
    1. Redirect to password update page when default password is not changed;
    2. Add username and password pattern validation;
    3. Add advanced page for factory reset and reboot;
    4. force http redirect to https
    
    Bug 15996

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 
 setup(name='weblcm-python',
       version='1.0',
-      py_modules=['__main__', 'weblcm_network', 'weblcm_log', 'weblcm_def', 'weblcm_swupdate', 'weblcm_users', 'weblcm_files']
+      py_modules=['__main__', 'weblcm_network', 'weblcm_log', 'weblcm_def', 'weblcm_swupdate', 'weblcm_users', 'weblcm_files', 'weblcm_advanced']
       )
