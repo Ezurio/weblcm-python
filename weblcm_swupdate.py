5edcba0aea4a4ac7faa0ff8b89f6b1134cad723f
sean.meng <Sean.Meng@lairdconnect.com>
2020-04-02 16:10:21 -0400

    weblcm-python: add update checking
    Customers have to implement any checking logic in weblcm_update_checking.sh
    before swupdate. Error message will be displayed on the web.
    
    Bug 15996

--- weblcm_swupdate.py
+++ weblcm_swupdate.py
@@ -1,10 +1,11 @@
 import cherrypy
 import dbus
-import subprocess
+import threading
 import os,errno
 import uuid
 import time
 import swclient
+from subprocess import Popen, PIPE, TimeoutExpired
 
 def octet_stream_in(force=True, debug=False):
 	request = cherrypy.serving.request
@@ -27,6 +28,9 @@ def octet_stream_in(force=True, debug=False):
 
 class SWUpdate:
 
+	_isUpdating = False
+	_lock = threading.Lock()
+
 	def __init__(self):
 		cherrypy.tools.octet_stream_in = cherrypy.Tool('before_request_body', octet_stream_in)
 
@@ -36,12 +40,45 @@ class SWUpdate:
 		pass
 
 	@cherrypy.expose
+	@cherrypy.tools.json_out()
 	def update_firmware_start(self):
+
 		dryrun = 0
-		swclient.prepare_fw_update(dryrun)
-		pass
+
+		result = {
+			'SDCERR': 1,
+			'message': "",
+		}
+
+		try:
+			proc = Popen("/usr/sbin/weblcm_update_checking.sh", stdout=PIPE, stderr=PIPE)
+			outs, errs = proc.communicate(timeout=cherrypy.request.app.config['weblcm'].get('swupdate_callback_timeout', 5))
+
+			if proc.returncode:
+				result['message'] = errs.decode("utf-8")
+				result['SDCERR'] = proc.returncode
+			else:
+				self._lock.acquire()
+				isupdating = self._isUpdating
+				self._isUpdating = True
+				self._lock.release()
+
+				if isupdating == False:
+					swclient.prepare_fw_update(dryrun)
+					result['SDCERR'] = 0
+				else:
+					result['message'] = "Device is busy"
+
+		except TimeoutExpired:
+			proc.kill()
+			outs, errs = proc.communicate()
+			result['message'] = "Update checking timeout"
+		except Exception as inst:
+			result['message'] = "An Error happened during update checking"
+
+		return result
 
 	@cherrypy.expose
 	def update_firmware_end(self):
 		swclient.end_fw_update()
-		pass
+		self._isUpdating = False
