79970eeb6ab9278dcc9d0a940aaf31cd6e73bf33
sean.meng <Sean.Meng@lairdconnect.com>
2020-05-19 10:41:27 -0400

    Add post check for swupdate
    1.Check bootside after swupate
    2.Creat a helper class to parse and save Network Status
    3.Improve the System version display Bug 15996, 16672
    
    Bug 15996

--- weblcm_update_checking.sh
+++ weblcm_update_checking.sh
@@ -7,19 +7,71 @@ exit_on_error() {
 	exit ${1}
 }
 
-systemctl restart swupdate
-systemctl -q is-active swupdate || exit_on_error 1 "Failed to start swupdate service"
+pre_update() {
 
-#Wait until swupdate is ready or timeout
-FILE=/tmp/sockinstctrl
-x=0
-while [ ${x} -lt 3 ]
-do
-	[[ -S ${FILE} ]] && break || sleep 1
-	x=$(( ${x} + 1 ))
-done
-[[ ${x} == 3 ]] && exit_on_error 1 "Failed to run swupdate service"
+	systemctl restart swupdate
+	systemctl -q is-active swupdate || exit_on_error 1 "Failed to start swupdate service"
 
+	#Wait until swupdate is ready or timeout
+	FILE=/tmp/sockinstctrl
+	x=0
+	while [ ${x} -lt 5 ]
+	do
+		[[ -S ${FILE} ]] && break || sleep 1
+		x=$(( ${x} + 1 ))
+	done
+	[[ ${x} == 5 ]] && exit_on_error 1 "Failed to run swupdate service"
+}
 
-#Success
-exit 0
+get_update() {
+
+	# Find our running ubiblock
+	set -- $(cat /proc/cmdline)
+	for x in "$@"; do
+		case "$x" in
+			ubi.block=*)
+			BLOCK=${x#*,}
+			;;
+		esac
+	done
+
+	x=0
+	while [ ${x} -lt 5 ]
+	do
+		bootside=`fw_printenv bootside -n`
+
+		if [ -z "$BLOCK" ] && [ ${bootside} == 'a' ]; then
+			break;
+		elif [ "$BLOCK" == 1 ] && [ ${bootside} == 'b' ]; then
+			break;
+		elif [ "$BLOCK" == 4 ] && [ ${bootside} == 'a' ]; then
+			break;
+		fi
+
+		sleep 1
+		x=$(( ${x} + 1 ))
+	done
+	[[ ${x} == 5 ]] && exit_on_error 1 "Bootside is not updated"
+}
+
+post_update() {
+	systemctl stop swupdate
+}
+
+case $1 in
+	pre-update)
+		pre_update
+		#Success
+		exit 0
+		;;
+	get-update)
+		get_update
+		#Success
+		exit 0
+		;;
+	post-update)
+		post_update
+		#Success
+		exit 0
+		;;
+esac
