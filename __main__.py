be69d119ceddd2f7e6bbfc66b805c8403e15e55f
sean.meng <Sean.Meng@lairdconnect.com>
2020-07-14 10:50:13 -0400

    weblcm-python: fix bugs
    including:
    -F5 redirects to login page
    -clickable card head on status page
    -save language option to localStorage to survives sessions
    
    Bug 16974, 16984, 16985,

--- __main__.py
+++ __main__.py
@@ -61,7 +61,7 @@ def force_session_checking():
 				"logSetting", "factoryReset", "reboot", "files", "datetime"
 			)
 
-	if not cherrypy.session._exists():
+	if not cherrypy.session.get('USERNAME', None):
 		url = cherrypy.url().split('/')[-1]
 		if url and ".html" not in url and ".js" not in url and any(path in url for path in paths):
 			raise cherrypy.HTTPError(401)
