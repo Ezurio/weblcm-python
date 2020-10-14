f5b227a3db0e50187327e153b2fe10cc3a19e9a3
sean.meng <Sean.Meng@lairdconnect.com>
2020-10-14 14:57:02 -0400

    weblcm-python: block users login
    Block login for 10 minutes after 5 unsuccessful log-in attempts
    within 10 minutes by default.
    
    Bug 17539

--- weblcm_settings.py
+++ weblcm_settings.py
@@ -142,7 +142,7 @@ class SystemSettingsManage(object):
 	@classmethod
 	def get_tamper_protection_timeout(cls):
 		'Unit: Second'
-		return int (WeblcmConfigManage.get_key_from_section(cls.section, 'tamper_protection_timeout', 60))
+		return int (WeblcmConfigManage.get_key_from_section(cls.section, 'tamper_protection_timeout', 600))
 
 	@classmethod
 	def get_max_web_clients(cls):
@@ -155,7 +155,11 @@ class SystemSettingsManage(object):
 
 	@classmethod
 	def get_login_retry_times(cls):
-		return int (WeblcmConfigManage.get_key_from_section(cls.section, 'login_retry_times', 3))
+		return int (WeblcmConfigManage.get_key_from_section(cls.section, 'login_retry_times', 5))
+
+	@classmethod
+	def get_login_retry_window(cls):
+		return int (WeblcmConfigManage.get_key_from_section(cls.section, 'login_retry_window', 600))
 
 	@classmethod
 	def get_log_data_streaming_size(cls):
