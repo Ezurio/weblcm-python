a066fa3b545045b68186700f570733897c4eefb9
Eric Bentley <eric.bentley@lairdconnect.com>
2021-10-04 14:08:29 -0400

    Update files interface: error handling/reporting
    BUG 17536

--- weblcm_files.py
+++ weblcm_files.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from pylibconfig import Config
 from threading import Lock
 from weblcm_settings import SystemSettingsManage
+from syslog import syslog
 
 @cherrypy.expose
 class FileManage(object):
@@ -32,7 +33,7 @@ class FileManage(object):
 				if not data:
 					break
 				out.write(data)
-			out.close();
+			out.close()
 		return path
 
 	def POST(self, *args, **kwargs):
@@ -40,29 +41,40 @@ class FileManage(object):
 		typ = kwargs.get('type', None)
 		fil = kwargs.get('file', None)
 		if not typ or not fil:
-			raise cherrypy.HTTPError(400) #bad request
+			if not typ:
+				syslog('FileManage POST - no type specified')
+			if not fil:
+				syslog('FileManage POST - no filename provided')
+			raise cherrypy.HTTPError(400, 'filename or type missing') #bad request
+
+		if typ not in weblcm_def.FILEDIR_DICT:
+			syslog(f'FileManage POST type {typ} unknown')
+			raise cherrypy.HTTPError(400, f'FileManage POST type {typ} unknown') #bad request
 
 		with FileManage._lock:
-			f = self.save_file(typ, fil)
-			if os.path.isfile(f):
-				if f.endswith(".zip"):
+			fp = self.save_file(typ, fil)
+			if os.path.isfile(fp):
+				if fp.endswith(".zip"):
 					password = kwargs.get('password', "")
 					p = subprocess.Popen([
-						FileManage.FILE_MANAGE_SCRIPT, typ, "unzip", f, weblcm_def.FILEDIR_DICT.get(typ), password
+						FileManage.FILE_MANAGE_SCRIPT, typ, "unzip", fp, weblcm_def.FILEDIR_DICT.get(typ), password
 					])
 					res = p.wait()
-					os.remove(f)
+					os.remove(fp)
 					if res:
-						raise cherrypy.HTTPError(500) #Internal server error
+						syslog(f"unzip command file '{fp}' failed with error {res}")
+						raise cherrypy.HTTPError(500, f'unzip command failed to unzip provided file.  Error returned: {res}') #Internal server error
 				return
+		syslog(f"unable to obtain FileManage._lock")
 
-		raise cherrypy.HTTPError(500) #Internal server error
+		raise cherrypy.HTTPError(500, 'unable to obtain internal file lock') #Internal server error
 
 	def GET(self, *args, **kwargs):
 
 		typ = kwargs.get('type', None)
 		if not typ:
-			raise cherrypy.HTTPError(400)
+			syslog('FileManage Get - no filename provided')
+			raise cherrypy.HTTPError(400, 'no filename provided')
 
 		fil = '{0}{1}'.format(typ, ".zip")
 		path = '{0}{1}'.format("/tmp/", fil)
@@ -71,29 +83,36 @@ class FileManage(object):
 
 			password = kwargs.get('password', None)
 			if not password:
-				raise cherrypy.HTTPError(400)
+				syslog('FileManage Get - no password provided')
+				raise cherrypy.HTTPError(400, 'no password provided')
 			p = subprocess.Popen([
 				FileManage.FILE_MANAGE_SCRIPT, "config", "zip",
 				weblcm_def.FILEDIR_DICT.get(typ), path, password
 			])
+			syslog("Configuration zipped for user")
 
 		elif typ == "log":
 
 			password = kwargs.get('password', None)
 			if not password:
-				raise cherrypy.HTTPError(400)
+				syslog('FileManage Get - no password provided')
+				raise cherrypy.HTTPError(400, 'no password provided')
 			p = subprocess.Popen([
 				FileManage.FILE_MANAGE_SCRIPT, "log", "zip",
 				FileManage._log_data_dir, path, password
 			])
-			p.wait()
+			syslog("System log zipped for user")
 
-		else:
+		elif typ == "debug":
 			p = subprocess.Popen([
 				FileManage.FILE_MANAGE_SCRIPT, "debug", "zip",
 				' '.join([FileManage._log_data_dir, weblcm_def.FILEDIR_DICT.get('config')]),
 				path, SystemSettingsManage.get_cert_for_file_encryption()
 			])
+			syslog("Configuration and system log zipped/encrypted for user")
+		else:
+			syslog(f"FileManage GET - unknown file type {typ}")
+			raise cherrypy.HTTPError(400, f'unknown file type {typ}')
 		p.wait()
 
 		if os.path.isfile(path):
@@ -101,22 +120,36 @@ class FileManage(object):
 			os.unlink(path);
 			return objFile;
 
-		raise cherrypy.HTTPError(500)
+		syslog(f"Failed to create file {path} for user")
+		raise cherrypy.HTTPError(500, f'failed to create file {path}')
 
 	@cherrypy.tools.json_out()
 	def DELETE(self, *args, **kwargs):
-		result = { 'SDCERR': weblcm_def.WEBLCM_ERRORS.get('SDCERR_FAIL') }
+		result = { 
+			'SDCERR': weblcm_def.WEBLCM_ERRORS.get('SDCERR_FAIL'),
+			'ErrorMsg': 'Unable to delete file'
+			}
 
 		typ = kwargs.get('type', None)
 		fil = kwargs.get('file', None)
 		if not typ or not fil:
-			raise cherrypy.HTTPError(400) #bad request
+			if not typ:
+				syslog('FileManage DELETE - no type specified')
+			if not fil:
+				syslog('FileManage DELETE - no filename provided')
+			raise cherrypy.HTTPError(400, 'missing type or filename') #bad request
 
 		path = os.path.normpath(os.path.join(weblcm_def.FILEDIR_DICT.get(typ), fil))
 		if os.path.isfile(path):
 			os.remove(path);
-		if not os.path.exists(path):
-			result['SDCERR'] = weblcm_def.WEBLCM_ERRORS.get('SDCERR_SUCCESS')
+			if not os.path.exists(path):
+				result['SDCERR'] = weblcm_def.WEBLCM_ERRORS.get('SDCERR_SUCCESS')
+				result['ErrorMsg'] = f'file {file} deleted'
+				syslog(f'file {file} deleted')
+			else:
+				syslog(f'Attempt to remove file {path} did not succeed')
+		else:
+			syslog(f'Attempt to remove non-existant file {path}')
 		return result
 
 @cherrypy.expose
@@ -127,7 +160,8 @@ class FilesManage(object):
 
 		typ = kwargs.get('type', None)
 		if not typ:
-			raise cherrypy.HTTPError(400)
+			syslog('FilesManage GET - no type provided')
+			raise cherrypy.HTTPError(400, 'no filename provided')
 
 		files = []
 		with os.scandir(weblcm_def.FILEDIR_DICT.get(typ)) as listOfEntries:
@@ -149,9 +183,10 @@ class AWMCfgManage(object):
 
 		#Infinite geo-location checks by default
 		result = { 'SDCERR': weblcm_def.WEBLCM_ERRORS.get('SDCERR_SUCCESS') }
-		result['geolocation_scanning_enable'] = 1
+		result['geolocation_scanning_enable'] = 0
+		result['ErrorMsg'] = 'AWM unsupported'
 
-		f = cherrypy.request.app.config['weblcm'].get('awm_cfg', None);
+		f = cherrypy.request.app.config['weblcm'].get('awm_cfg', None)
 		if not f:
 			return result
 
@@ -163,6 +198,7 @@ class AWMCfgManage(object):
 			config.readFile(f)
 			if config.exists("scan_attempts"):
 				result['geolocation_scanning_enable'] = config.value("scan_attempts")[0]
+				result['ErrorMesg'] = ''
 
 		return result
 
@@ -176,31 +212,33 @@ class AWMCfgManage(object):
 		#others: enable geolocation scanning
 
 		result = { 'SDCERR': weblcm_def.WEBLCM_ERRORS.get('SDCERR_FAIL') }
-		result['geolocation_scanning_enable'] = 1
+		result['geolocation_scanning_enable'] = 0
+		result['ErrorMsg'] = 'AWM unsupported'
 
-		f = cherrypy.request.app.config['weblcm'].get('awm_cfg', None);
-		if not f:
+		fp = cherrypy.request.app.config['weblcm'].get('awm_cfg', None);
+		if not fp:
 			return result
 
-		d = Path(os.path.dirname(f))
+		d = Path(os.path.dirname(fp))
 		d.mkdir(exist_ok=True)
 
 		geolocation_scanning_enable = cherrypy.request.json.get('geolocation_scanning_enable', 0)
 
 		config = Config()
 		with AWMCfgManage._lock:
-			if os.path.isfile(f):
-				config.readFile(f)
+			if os.path.isfile(fp):
+				config.readFile(fp)
 			if geolocation_scanning_enable:
 				if config.exists("scan_attempts"):
 					config.remove("", "scan_attempts")
-					config.writeFile(f)
+					config.writeFile(fp)
 			else:
 				if not config.exists("scan_attempts"):
 					config.addInteger("", "scan_attempts")
 				config.setValue("scan_attempts", geolocation_scanning_enable)
-				config.writeFile(f)
+				config.writeFile(fp)
 
 		result['geolocation_scanning_enable'] = geolocation_scanning_enable
 		result['SDCERR'] = weblcm_def.WEBLCM_ERRORS.get('SDCERR_SUCCESS')
+		result['ErrorMsg']= ''
 		return result
