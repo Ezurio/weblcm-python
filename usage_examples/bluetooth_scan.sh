814127768ab6b6591cf7336e019b79f2ac251daa
Erik Strack <erik.strack@lairdconnect.com>
2021-11-01 18:37:06 -0400

    Add Bluetooth RESTful API examples.
    example curl scripts and readme file demonstrating use of API
    
    Add smart_jq routine to bypass JSON decode in the event of non-JSON data present.
    
    Fixed a couple typos.

---
+++ usage_examples/bluetooth_scan.sh
@@ -0,0 +1,38 @@
+source global_settings
+
+echo -e "\n========================="
+echo "Bluetooth scan"
+
+echo -e "reset controller, clear cache and force fresh scan:\n"
+${CURL_APP} --location --request PUT ${URL}/bluetooth/${BT_CONTROLLER} \
+    --header "Content-Type: application/json" \
+    -b cookie --insecure\
+    --data '{"powered": 0, "discovering": 0, "discoverable": 0}' \
+    | ${JQ_APP} --raw-output
+echo -e '\n'
+
+echo -e "\nscan:\n"
+${CURL_APP} --location --request PUT ${URL}/bluetooth/${BT_CONTROLLER} \
+    --header "Content-Type: application/json" \
+    -b cookie --insecure\
+    --data '{"powered": 1, "discovering": 1, "discoverable": 1}' \
+    | ${JQ_APP}
+echo -e '\n'
+
+echo -e "\nconfirm:\n"
+${CURL_APP} --location --request GET ${URL}/bluetooth/${BT_CONTROLLER}?filter=powered,discovering,discoverable \
+    --header "Content-Type: application/json" \
+    -b cookie --insecure\
+    | ${JQ_APP}
+echo -e '\n'
+
+for i in {1..5}; do
+    sleep 5
+    reset
+    echo -e "\nresults:\n"
+    ${CURL_APP} --location --request GET ${URL}/bluetooth/${BT_CONTROLLER}?filter=bluetoothDevices \
+        -b cookie --insecure\
+        | ${JQ_APP}
+done
+
+echo -e "\n"
