fec30a021726f25f5dc5817f4f25ce7ee05334dd
Erik Strack <erik.strack@lairdconnect.com>
2022-06-09 16:53:21 -0400

    weblcm-python: usage_examples: update firmware update scripts
    This updates firmware update scripts for changes in "fixes for operating swupdate in daemon mode".
    
    Bug 21883

--- usage_examples/firmware_update-server-pull.sh
+++ usage_examples/firmware_update-server-pull.sh
@@ -1,4 +1,9 @@
-FIRMWARE=http://192.168.1.123:8080/som60.swu
+FIRMWARE="${1}"
+
+if [ -z "${FIRMWARE}" ]; then
+    echo usage: ${0} firmware url, e.g. http://192.168.1.123:8080/som60.swu
+    exit
+fi
 
 source global_settings
 
@@ -6,31 +11,35 @@ source global_settings
 echo -e "\n\n========================="
 echo "Firmware update"
 echo "========================="
+echo "Requesting device pull FW from ${FIRMWARE}..."
+echo
 ${CURL_APP} -s --header "Content-Type: application/json" \
     --request POST   --data \
     '{"image":"full", "url":"'"${FIRMWARE}"'"}'  --insecure \
     ${URL}/firmware -b cookie
 
-echo && sleep 5
-${CURL_APP} -s --request GET --insecure ${URL}/firmware?mode=1 -b cookie
-echo && sleep 5
-${CURL_APP} -s --request GET --insecure ${URL}/firmware?mode=1 -b cookie
-echo && sleep 5
-${CURL_APP} -s --request GET --insecure ${URL}/firmware?mode=1 -b cookie
-echo && sleep 5
-${CURL_APP} -s --request GET --insecure ${URL}/firmware?mode=1 -b cookie
-echo && sleep 5
-${CURL_APP} -s --request GET --insecure ${URL}/firmware?mode=1 -b cookie
-echo && sleep 5
-${CURL_APP} -s --request GET --insecure ${URL}/firmware?mode=1 -b cookie
-echo && sleep 5
-${CURL_APP} -s --request GET --insecure ${URL}/firmware?mode=1 -b cookie
-echo "You will need to reboot the device once the update is complete"
+SUCCESS=false
+echo
+echo
+while true; do
+    echo "Checking status:"
+    ${CURL_APP} -s --request GET --insecure ${URL}/firmware -b cookie | tee status | ${JQ_APP}
+    echo
+    if grep -q Updated status; then
+        SUCCESS=true
+        break
+    fi
+    if grep -q Failed status; then
+        break
+    fi
+    sleep 1
+done
 
-
-${CURL_APP} -s --request DELETE --insecure ${URL}/firmware -b cookie
+echo
+if [ "${SUCCESS}" = true ]; then
+    source ./reboot_put.sh
+fi
 
 
 echo ""
 echo "Done"
-
