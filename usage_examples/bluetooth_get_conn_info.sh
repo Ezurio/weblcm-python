5d227e23680cf49c52c7b84998e5d228cbea3a48
Erik Strack <erik.strack@lairdconnect.com>
2022-06-28 09:55:35 -0400

    weblcm-python: Bluetooth: add getConnInfo device command
    getConnInfo can be used to fetch the RSSI, tx power, and max tx power associated with Bluetooth low-energy or BR/EDR links.
    
    Bug 22175

---
+++ usage_examples/bluetooth_get_conn_info.sh
@@ -0,0 +1,26 @@
+source global_settings
+
+echo -e "\n========================="
+echo "Bluetooth Get Connection Info (GetConnInfo)"
+
+if [ -z "$BT_DEVICE" ]
+then
+    echo "Please invoke with Bluetooth device address set e.g. BT_DEVICE=xx:xx:xx:xx:xx:xx $0"
+    exit
+fi
+
+echo -e "\nBluetooth Get Connection Info:\n"
+${CURL_APP} --location --request PUT ${URL}/bluetooth/${BT_CONTROLLER}/${BT_DEVICE} \
+    --header "Content-Type: application/json" \
+    -b cookie --insecure\
+    --data '{"command": "getConnInfo"}' \
+    | ${JQ_APP}
+echo -e '\n'
+
+# Note reported RSSI may differ due to minimum RSSI difference to update device cached value logic.
+echo -e "\nread Bluetooth state:\n"
+${CURL_APP} --location --request GET ${URL}/bluetooth/${BT_CONTROLLER}/${BT_DEVICE} \
+    --header "Content-Type: application/json" \
+    -b cookie --insecure\
+    | ${JQ_APP} | grep --color --context=99 -i rssi
+echo -e '\n'
