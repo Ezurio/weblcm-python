b50458302d20378264b4eb639b835341bdcd1051
Chris Trowbridge <chris.trowbridge@lairdconnect.com>
2022-07-11 22:01:46 -0400

    weblcm-python: Add extended NetworkManager profile settings retrieval endpoint
    This change extends the /connection endpoint to support an optional 'extended'
    parameter. When enabled, the endpoint utilizes libnm and gobject introspection
    to retrieve detail connection profile settings similar to what is available
    from nmcli.
    
    Bug 22029

--- usage_examples/connection_get.sh
+++ usage_examples/connection_get.sh
@@ -1,12 +1,18 @@
 UUID="${UUID:-"a760e87e-e23a-4ea6-81f9-c4e29052db27"}"
+EXTENDED="${EXTENDED:-false}"
 source global_settings
 
 
 echo -e "\n========================="
 echo "Get connection"
 
+REQUEST_URL="${URL}/connection?uuid=${UUID}"
+if [ "${EXTENDED}" == true ]; then
+    REQUEST_URL="${REQUEST_URL}&extended=true"
+fi
+
 ${CURL_APP} -s --location \
-    --request GET "${URL}/connection?uuid=${UUID}" \
+    --request GET "${REQUEST_URL}" \
     -b cookie --insecure \
 | ${JQ_APP}
 
