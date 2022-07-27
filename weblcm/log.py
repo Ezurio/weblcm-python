6735be23203a5dacd2e4f8923b24629df3bb7200
Chris Trowbridge <chris.trowbridge@lairdconnect.com>
2022-07-27 09:36:11 -0400

    weblcm-python: Add support for controlling journal log forwarding
    This change adds support for controlling and querying the active state of the
    systemd journal log forwarding service (systemd-journal-gatewayd). The
    service's active state is read and set via the systemd D-Bus API.
    
    The desired behavior is that the log forwarding service is disabled by default,
    but, after it is enabled, the service should be persistently activated at boot
    by systemd. To achieve this, an empty 'flag file'
    (/data/secret/weblcm-python/log_forwarding_enabled) is created/removed  which
    can be checked by systemd as a pre-start condition (ConditionPathExists) for the
    journal forwarding service.
    
    Bug 22085

--- weblcm/log.py
+++ weblcm/log.py
@@ -1,9 +1,24 @@
+import os
 from syslog import LOG_ERR, syslog
 import cherrypy
 import dbus
 import time
 from systemd import journal
-from . import definition
+from .definition import (
+    LOG_FORWARDING_ENABLED_FLAG_FILE,
+    SYSTEMD_BUS_NAME,
+    SYSTEMD_MAIN_OBJ,
+    SYSTEMD_MANAGER_IFACE,
+    SYSTEMD_UNIT_ACTIVE_STATE_PROP,
+    SYSTEMD_UNIT_IFACE,
+    SYSTEMD_UNIT_UNIT_FILE_STATE_PROP,
+    WEBLCM_ERRORS,
+    WPA_IFACE,
+    WPA_OBJ,
+    WIFI_DRIVER_DEBUG_PARAM,
+    DBUS_PROP_IFACE,
+    SYSTEMD_JOURNAL_GATEWAYD_SOCKET_FILE,
+)
 
 
 @cherrypy.expose
@@ -72,6 +87,215 @@ class LogData(object):
 
 
 @cherrypy.expose
+class LogForwarding(object):
+    @property
+    def active_state(self) -> str:
+        """
+        The current 'ActiveState' value for the 'systemd-journal-gatewayd.socket' (log forwarding)
+        service as a string. Possible values are:
+        - active
+        - reloading
+        - inactive
+        - failed
+        - activating
+        - deactivating
+        - unknown (error state added by us)
+
+        See below for more info:
+        https://www.freedesktop.org/software/systemd/man/org.freedesktop.systemd1.html
+        """
+        try:
+            bus = dbus.SystemBus()
+            manager = dbus.Interface(
+                bus.get_object(SYSTEMD_BUS_NAME, SYSTEMD_MAIN_OBJ),
+                SYSTEMD_MANAGER_IFACE,
+            )
+            socket_unit_props = dbus.Interface(
+                bus.get_object(
+                    SYSTEMD_BUS_NAME,
+                    manager.LoadUnit(SYSTEMD_JOURNAL_GATEWAYD_SOCKET_FILE),
+                ),
+                DBUS_PROP_IFACE,
+            )
+            active_state = socket_unit_props.Get(
+                SYSTEMD_UNIT_IFACE, SYSTEMD_UNIT_ACTIVE_STATE_PROP
+            )
+            return active_state
+        except Exception as e:
+            syslog(
+                LOG_ERR,
+                f"Could not read 'ActiveState' of {SYSTEMD_JOURNAL_GATEWAYD_SOCKET_FILE}: {str(e)}",
+            )
+            return "unknown"
+
+    @property
+    def unit_file_state(self) -> str:
+        """
+        The current 'UnitFileState' value for the 'systemd-journal-gatewayd.socket' (log forwarding)
+        service as a string. Possible values are:
+        - enabled
+        - enabled-runtime
+        - linked
+        - linked-runtime
+        - masked
+        - masked-runtime
+        - static
+        - disabled
+        - invalid
+        - unknown (error state added by us)
+
+        See below for more info:
+        https://www.freedesktop.org/software/systemd/man/org.freedesktop.systemd1.html
+        """
+        try:
+            bus = dbus.SystemBus()
+            manager = dbus.Interface(
+                bus.get_object(SYSTEMD_BUS_NAME, SYSTEMD_MAIN_OBJ),
+                SYSTEMD_MANAGER_IFACE,
+            )
+            socket_unit_props = dbus.Interface(
+                bus.get_object(
+                    SYSTEMD_BUS_NAME,
+                    manager.LoadUnit(SYSTEMD_JOURNAL_GATEWAYD_SOCKET_FILE),
+                ),
+                DBUS_PROP_IFACE,
+            )
+            active_state = socket_unit_props.Get(
+                SYSTEMD_UNIT_IFACE, SYSTEMD_UNIT_UNIT_FILE_STATE_PROP
+            )
+            return active_state
+        except Exception as e:
+            syslog(
+                LOG_ERR,
+                f"Could not read 'UnitFileState' of {SYSTEMD_JOURNAL_GATEWAYD_SOCKET_FILE}: {str(e)}",
+            )
+            return "unknown"
+
+    @classmethod
+    def activate(self) -> bool:
+        """
+        Activate the log forwarding service (systemd-journal-gatewayd.socket)
+        """
+        try:
+            bus = dbus.SystemBus()
+            manager = dbus.Interface(
+                bus.get_object(SYSTEMD_BUS_NAME, SYSTEMD_MAIN_OBJ),
+                SYSTEMD_MANAGER_IFACE,
+            )
+            manager.StartUnit(SYSTEMD_JOURNAL_GATEWAYD_SOCKET_FILE, "replace")
+            return True
+        except Exception as e:
+            syslog(
+                LOG_ERR,
+                f"Could not activate the log forwarding service: {str(e)}",
+            )
+            return False
+
+    @classmethod
+    def deactivate(self) -> bool:
+        """
+        Deactivate the log forwarding service (systemd-journal-gatewayd.socket)
+        """
+        try:
+            bus = dbus.SystemBus()
+            manager = dbus.Interface(
+                bus.get_object(SYSTEMD_BUS_NAME, SYSTEMD_MAIN_OBJ),
+                SYSTEMD_MANAGER_IFACE,
+            )
+            manager.StopUnit(SYSTEMD_JOURNAL_GATEWAYD_SOCKET_FILE, "replace")
+            return True
+        except Exception as e:
+            syslog(
+                LOG_ERR,
+                f"Could not deactivate the log forwarding service: {str(e)}",
+            )
+            return False
+
+    @cherrypy.tools.json_out()
+    def GET(self):
+        result = {
+            "SDCERR": WEBLCM_ERRORS["SDCERR_FAIL"],
+            "InfoMsg": "Could not retrieve log forwarding state",
+            "state": "unknown",
+        }
+
+        try:
+            result["state"] = self.active_state
+            if result["state"] != "unknown":
+                result["SDCERR"] = WEBLCM_ERRORS["SDCERR_SUCCESS"]
+                result["InfoMsg"] = ""
+        except Exception as e:
+            syslog(LOG_ERR, f"Could not retrieve log forwarding state: {str(e)}")
+
+        return result
+
+    @cherrypy.tools.accept(media="application/json")
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def PUT(self):
+        result = {
+            "SDCERR": WEBLCM_ERRORS["SDCERR_FAIL"],
+            "InfoMsg": "Could not set log forwarding state",
+        }
+
+        try:
+            valid_states = ["active", "inactive"]
+
+            post_data = cherrypy.request.json
+            requested_state = post_data.get("state", None)
+            if requested_state not in valid_states:
+                result[
+                    "InfoMsg"
+                ] = f"Invalid state: {requested_state}; valid states: {valid_states}"
+                return result
+
+            # Read the current 'ActiveState' of the log forwarding service
+            current_state = self.active_state
+
+            if requested_state == "active":
+                # Create the 'flag file' which systemd uses to determine if it should start the
+                # systemd-journal-gatewayd.socket unit.
+                with open(LOG_FORWARDING_ENABLED_FLAG_FILE, "w"):
+                    pass
+
+                if current_state == "active":
+                    # Service already active
+                    result["InfoMsg"] = "Log forwarding already active"
+                    result["SDCERR"] = WEBLCM_ERRORS["SDCERR_SUCCESS"]
+                else:
+                    # Activate service
+                    if self.activate():
+                        result["InfoMsg"] = "Log forwarding activated"
+                        result["SDCERR"] = WEBLCM_ERRORS["SDCERR_SUCCESS"]
+            elif requested_state == "inactive":
+                # Remove the 'flag file' which systemd uses to determine if it should start the
+                # systemd-journal-gatewayd.socket unit.
+                try:
+                    os.remove(LOG_FORWARDING_ENABLED_FLAG_FILE)
+                except OSError:
+                    # Handle the case where the file isn't already present
+                    pass
+
+                if current_state == "inactive":
+                    # Service is already inactive
+                    result["InfoMsg"] = "Log forwarding already inactive"
+                    result["SDCERR"] = WEBLCM_ERRORS["SDCERR_SUCCESS"]
+                else:
+                    # Deactivate service
+                    if self.deactivate():
+                        result["InfoMsg"] = "Log forwarding deactivated"
+                        result["SDCERR"] = WEBLCM_ERRORS["SDCERR_SUCCESS"]
+        except Exception as e:
+            syslog(LOG_ERR, f"Could not set log forwarding state: {str(e)}")
+            result = {
+                "SDCERR": WEBLCM_ERRORS["SDCERR_FAIL"],
+                "InfoMsg": "Could not set log forwarding state",
+            }
+
+        return result
+
+
+@cherrypy.expose
 class LogSetting(object):
     @cherrypy.tools.accept(media="application/json")
     @cherrypy.tools.json_in()
@@ -95,9 +319,9 @@ class LogSetting(object):
 
         try:
             bus = dbus.SystemBus()
-            proxy = bus.get_object(definition.WPA_IFACE, definition.WPA_OBJ)
-            wpas = dbus.Interface(proxy, definition.DBUS_PROP_IFACE)
-            wpas.Set(definition.WPA_IFACE, "DebugLevel", supp_level)
+            proxy = bus.get_object(WPA_IFACE, WPA_OBJ)
+            wpas = dbus.Interface(proxy, DBUS_PROP_IFACE)
+            wpas.Set(WPA_IFACE, "DebugLevel", supp_level)
         except Exception as e:
             result["InfoMsg"] = "unable to set supplicant debug level"
             return result
@@ -114,7 +338,7 @@ class LogSetting(object):
             return result
 
         try:
-            driver_debug_file = open(definition.WIFI_DRIVER_DEBUG_PARAM, "w")
+            driver_debug_file = open(WIFI_DRIVER_DEBUG_PARAM, "w")
             if driver_debug_file.mode == "w":
                 driver_debug_file.write(str(drv_level))
         except Exception as e:
@@ -134,16 +358,16 @@ class LogSetting(object):
 
         try:
             bus = dbus.SystemBus()
-            proxy = bus.get_object(definition.WPA_IFACE, definition.WPA_OBJ)
-            wpas = dbus.Interface(proxy, definition.DBUS_PROP_IFACE)
-            debug_level = wpas.Get(definition.WPA_IFACE, "DebugLevel")
+            proxy = bus.get_object(WPA_IFACE, WPA_OBJ)
+            wpas = dbus.Interface(proxy, DBUS_PROP_IFACE)
+            debug_level = wpas.Get(WPA_IFACE, "DebugLevel")
             result["suppDebugLevel"] = debug_level
         except Exception as e:
             result["Errormsg"] = "Unable to determine supplicant debug level"
             result["SDCERR"] = 1
 
         try:
-            driver_debug_file = open(definition.WIFI_DRIVER_DEBUG_PARAM, "r")
+            driver_debug_file = open(WIFI_DRIVER_DEBUG_PARAM, "r")
             if driver_debug_file.mode == "r":
                 contents = driver_debug_file.read(1)
                 result["driverDebugLevel"] = contents
