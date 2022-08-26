0b96561d2310dec6807ff1eec3aa4d1166389287
Chris Trowbridge <chris.trowbridge@lairdconnect.com>
2022-08-26 09:50:04 -0400

    weblcm-python: Update method for reading journal logs
    This change removes the use and dependency on the aging and infrequently
    maintained python-systemd package (the most recent release targets systemd 234,
    and we're currently using systemd 250).
    
    The python-systemd package was being used to read the journal logs as JSON, but
    this change switches to using journalctl itself with the built-in switch
    '--output=json'.
    
    Additionally, the python example script to read the journal logs which also
    utilized the python-systemd package has been removed.
    
    Lastly, a couple small formatting fixes as suggested by the linting tool Flake8
    were made.
    
    Bug 22304

--- weblcm/log.py
+++ weblcm/log.py
@@ -1,9 +1,12 @@
+import json
 import os
 from syslog import LOG_ERR, syslog
 import cherrypy
 import dbus
 import time
-from systemd import journal
+from datetime import datetime
+from .settings import SystemSettingsManage
+from subprocess import run
 from .definition import (
     LOG_FORWARDING_ENABLED_FLAG_FILE,
     SYSTEMD_BUS_NAME,
@@ -27,9 +30,7 @@ class LogData(object):
     @cherrypy.tools.json_out()
     def GET(self, *args, **kwargs):
         result = {"SDCERR": 0, "InfoMsg": ""}
-        logs = []
 
-        reader = journal.Reader()
         try:
             priority = int(kwargs.get("priority", 7))
         except Exception as e:
@@ -39,7 +40,6 @@ class LogData(object):
             return {"SDCERR": 1, "InfoMsg": "Priority must be an int between 0-7"}
         if priority not in range(0, 8, 1):
             return {"SDCERR": 1, "InfoMsg": "Priority must be an int between 0-7"}
-        reader.log_level(priority)
         # use .lower() to ensure incoming type has comparable case
         typ = kwargs.get("type", "All").lower()
         # TODO - documentation says 'python' is lower case while others are upper/mixed case.
@@ -61,29 +61,64 @@ class LogData(object):
                 "SDCERR": 1,
                 "InfoMsg": f"supplied type parameter must be one of {str(types)}",
             }
-        if typ != "All":
-            reader.add_match(SYSLOG_IDENTIFIER=typ)
         try:
             days = int(kwargs.get("days", 1))
         except Exception as e:
             syslog(LOG_ERR, f"Error parsing 'days' parameter as an integer: {str(e)}")
             return {"SDCERR": 1, "InfoMsg": "days must be an int"}
-        if days > 0:
-            reader.seek_realtime(time.time() - days * 86400)
 
-        for entry in reader:
-            log = {}
-            log["time"] = str(entry.get("__REALTIME_TIMESTAMP", "Undefined"))
-            log["priority"] = str(entry.get("PRIORITY", 7))
-            log["identifier"] = entry.get("SYSLOG_IDENTIFIER", "Undefined")
-            log["message"] = entry.get("MESSAGE", "Undefined")
-            logs.append(log)
+        try:
+            journalctl_args = [
+                "journalctl",
+                f"--priority={str(priority)}",
+                "--output=json",
+            ]
+            if typ != "All":
+                journalctl_args.append(f"--identifier={str(typ)}")
+            if days > 0:
+                journalctl_args.append(
+                    f"--since={datetime.fromtimestamp(time.time() - days * 86400).strftime('%Y-%m-%d %H:%M:%S')}"
+                )
 
-        result["InfoMsg"] = f"type: {typ}; days: {days}; Priority: {priority}"
-        result["count"] = len(logs)
-        result["log"] = logs
-        reader.close()
-        return result
+            proc = run(
+                journalctl_args,
+                capture_output=True,
+                timeout=SystemSettingsManage.get_user_callback_timeout(),
+            )
+            if proc.returncode == 0:
+                logs = []
+                for line in str(proc.stdout.decode("utf-8")).split("\n"):
+                    if line.strip() == "":
+                        # The last line is empty, so break if we see it
+                        break
+                    entry = json.loads(line)
+                    log = {}
+                    timestamp = str(entry.get("__REALTIME_TIMESTAMP", "Undefined"))
+                    log["time"] = (
+                        datetime.fromtimestamp(float(timestamp) / 1000000).strftime(
+                            "%Y-%m-%d %H:%M:%S.%f"
+                        )
+                        if timestamp != "Undefined"
+                        else "Undefined"
+                    )
+                    log["priority"] = str(entry.get("PRIORITY", 7))
+                    log["identifier"] = entry.get("SYSLOG_IDENTIFIER", "Undefined")
+                    log["message"] = entry.get("MESSAGE", "Undefined")
+                    logs.append(log)
+                result["InfoMsg"] = f"type: {typ}; days: {days}; Priority: {priority}"
+                result["count"] = len(logs)
+                result["log"] = logs
+                return result
+            else:
+                syslog(
+                    LOG_ERR,
+                    f"journalctl error - returncode: {str(proc.returncode)}, stderr: {str(proc.stderr.decode('utf-8'))}",
+                )
+                return {"SDCERR": 1, "InfoMsg": "Could not read journal logs"}
+
+        except Exception as e:
+            syslog(LOG_ERR, f"Could not read journal logs: {str(e)}")
+            return {"SDCERR": 1, "InfoMsg": "Could not read journal logs"}
 
 
 @cherrypy.expose
@@ -304,16 +339,16 @@ class LogSetting(object):
         result = {"SDCERR": 1, "InfoMsg": ""}
         post_data = cherrypy.request.json
 
-        if not "suppDebugLevel" in post_data:
+        if "suppDebugLevel" not in post_data:
             result["InfoMsg"] = "suppDebugLevel missing from JSON data"
             return result
-        if not "driverDebugLevel" in post_data:
+        if "driverDebugLevel" not in post_data:
             result["InfoMsg"] = "driverDebugLevel missing from JSON data"
             return result
 
         levels = {"none", "error", "warning", "info", "debug", "msgdump", "excessive"}
         supp_level = post_data.get("suppDebugLevel").lower()
-        if not supp_level in levels:
+        if supp_level not in levels:
             result["InfoMsg"] = f"suppDebugLevel must be one of {levels}"
             return result
 
@@ -323,13 +358,14 @@ class LogSetting(object):
             wpas = dbus.Interface(proxy, DBUS_PROP_IFACE)
             wpas.Set(WPA_IFACE, "DebugLevel", supp_level)
         except Exception as e:
+            syslog(LOG_ERR, f"unable to set supplicant debug level: {str(e)}")
             result["InfoMsg"] = "unable to set supplicant debug level"
             return result
 
         drv_level = post_data.get("driverDebugLevel")
         try:
             drv_level = int(drv_level)
-        except Exception as e:
+        except Exception:
             result["InfoMsg"] = "driverDebugLevel must be 0 or 1"
             return result
 
@@ -342,6 +378,7 @@ class LogSetting(object):
             if driver_debug_file.mode == "w":
                 driver_debug_file.write(str(drv_level))
         except Exception as e:
+            syslog(LOG_ERR, f"unable to set driver debug level: {str(e)}")
             result["InfoMsg"] = "unable to set driver debug level"
             return result
 
@@ -363,6 +400,7 @@ class LogSetting(object):
             debug_level = wpas.Get(WPA_IFACE, "DebugLevel")
             result["suppDebugLevel"] = debug_level
         except Exception as e:
+            syslog(LOG_ERR, f"Unable to determine supplicant debug level: {str(e)}")
             result["Errormsg"] = "Unable to determine supplicant debug level"
             result["SDCERR"] = 1
 
@@ -372,6 +410,7 @@ class LogSetting(object):
                 contents = driver_debug_file.read(1)
                 result["driverDebugLevel"] = contents
         except Exception as e:
+            syslog(LOG_ERR, f"Unable to determine driver debug level: {str(e)}")
             if result.get("SDCERR") == 0:
                 result["Errormsg"] = "Unable to determine driver debug level"
             else:
