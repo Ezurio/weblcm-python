b546b324383d4a30cd72488340d180547991b351
Service Build Slave <it.devops@lairdconnect.com>
2023-06-16 19:33:59 +0000

    bump to build number 11.0.0.52

--- weblcm/definition.py
+++ weblcm/definition.py
@@ -1,6 +1,6 @@
 import os
 
-WEBLCM_PYTHON_VERSION = "11.0.0.51"
+WEBLCM_PYTHON_VERSION = "11.0.0.52"
 
 # TODO - deal with directories for file retrieval (see FIELDIR_DICT and files.py)
 SYSTEM_CONF_DIR = "/data/"
