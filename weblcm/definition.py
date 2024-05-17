0e35c9c9e2a140ff2fa2cb21b1004923637b3e74
Jenkins <jenkins@ip-10-16-82-7.us-east-2.compute.internal>
2024-05-17 09:32:40 +0000

    bump to build number 11.236.0.29

--- weblcm/definition.py
+++ weblcm/definition.py
@@ -1,6 +1,6 @@
 import os
 
-WEBLCM_PYTHON_VERSION = "11.236.0.28"
+WEBLCM_PYTHON_VERSION = "11.236.0.29"
 
 # TODO - deal with directories for file retrieval (see FIELDIR_DICT and files.py)
 SYSTEM_CONF_DIR = "/etc"
