a55521e14039752090a83914ab8643d8b6dedb43
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-05-17 21:32:58 -0400

    Updated version
    PROD-10246

--- weblcm/definition.py
+++ weblcm/definition.py
@@ -1,6 +1,6 @@
 import os
 
-WEBLCM_PYTHON_VERSION = "1.0.0.5"
+WEBLCM_PYTHON_VERSION = "0.0.0.0"
 
 # TODO - deal with directories for file retrieval (see FIELDIR_DICT and files.py)
 SYSTEM_CONF_DIR = "/etc"
