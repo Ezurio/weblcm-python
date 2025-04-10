db919c807e7465474f845649553be654dc852fff
Service Build Slave <it.devops@ezurio.com>
2025-04-10 19:29:21 +0000

    Bump to build number 12.178.0.1

--- weblcm/definition.py
+++ weblcm/definition.py
@@ -4,7 +4,7 @@
 #
 import os
 
-WEBLCM_PYTHON_VERSION = "12.0.0.103"
+WEBLCM_PYTHON_VERSION = "12.178.0.1"
 
 # TODO - deal with directories for file retrieval (see FIELDIR_DICT and files.py)
 SYSTEM_CONF_DIR = "/etc"
